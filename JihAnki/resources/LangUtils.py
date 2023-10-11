de05086355b7eda1f9a40ca0d334777db471fdba
Greg Seljak <gregoryseljak@gmail.com>
2023-10-11 13:54:54 -0400

    very nice EntryProcessing

--- JihAnki/resources/LangUtils.py
+++ JihAnki/resources/LangUtils.py
@@ -1,19 +1,24 @@
+#%%
 godan_ends = list("うくぐすつぬふぶむる")
 jpob = "（"
 jpcb = "）"
 
 katakana_a = "aアカガサザタダナハバパマヤラワ"
+#12449 to 
 katakana_i = "iイキギシジチヂニヒビピミリヰ"
 katakana_u = "uウクグスズツヅヌフブプムユル"
 katakana_e = "eエケゲセゼテデネヘベペメレヱ"
 katakana_o = "oオコゴソゾトドノホボポモヨロヲ"
 
-def standardize_parsed(inentry):
+intMIN_KATA=12448
+
+
+"""def standardize_parsed(inentry):
     if "-" in inentry:
         entry = inentry[:inentry.find("-")]
     else:
         entry = inentry
-    entry = entry.replace("為る", "する")
+    entry = entry.replace("為る", "する")"""
 
 def sym_to_dec(inchar:str):
     # from symbolic char to dec representation of unicode #
@@ -33,6 +38,12 @@ def just_kanji(inword:str):
             outlist.append(character)
     return outlist
 
+def is_kanji(inword:str):
+    for char in inword:
+        if sym_to_dec(char)<=19968:
+            return False
+    return True
+
 def is_hiragana(word:str, allow_white=True):
     # check if each char in string is hiragana
     for character in word:
@@ -42,14 +53,33 @@ def is_hiragana(word:str, allow_white=True):
         if kanadec < 12354 or kanadec>12435:
             return False
     return True
+#%%
+def is_katakana(word:str, allow_white=True):
+    # check if each char in string is katakana
+    for char in word:
+        try:
+            if char.isspace() and (allow_white is True):
+                continue
+            kanadec = sym_to_dec(char)
+            if kanadec < 12448 or kanadec > 12543:
+                return False
+        except ValueError: # not even kana
+            return False    
+    return True
 
+
+#%%
 def to_hiragana(inword:str):
     # like tolower(), but with hiragana
     outword = ""
     for inkana in inword:
+        if inkana=="ー":
+            outword+="ー"
+            continue
         if not inkana.isascii():
-            inkana_dec =sym_to_dec(inkana)
-            if inkana_dec >= 12448 and inkana_dec <= 12534:
+            inkana_dec=sym_to_dec(inkana)
+            #inkana_dec >= 12448 and inkana_dec <= 12534
+            if is_katakana(inkana):
                 outword += dec_to_sym(inkana_dec-96)
             else:
                 outword += inkana
@@ -57,21 +87,74 @@ def to_hiragana(inword:str):
             outword += inkana
     return outword
 
-def standardize_phonetic(_inParse):
-    outParse=to_hiragana(_inParse)
+def to_katakana(inword:str):
+    # like toupper(), but with katakana
+    outword = ""
+    for inkana in inword:
+        if not inkana.isascii():
+            inkana_dec=sym_to_dec(inkana)
+            #inkana_dec >= 12448 and inkana_dec <= 12534
+            if is_hiragana(inkana):
+                outword += dec_to_sym(inkana_dec+96)
+            else:
+                outword += inkana
+        else:
+            outword += inkana
+    return outword
+
+
+
+def standardize_phonetic(entry):
+    # returns the dictionary-form yomikata[hiragana]
+    # of the POS
+    outParse=entry[1]
     while "ー" in outParse:
         d = outParse.find("ー")
-        if d == 0:
-            print("bad d found at "+str(d))
-            insertChar="$"
-        elif outParse[d-1]=="お":
-            insertChar="う"
-        else:
-            insertChar=outParse[d-1]
+        insertChar=entry[2][d]
         outParse=outParse[:d]+insertChar+outParse[d+1:]
+    outParse=to_hiragana(outParse)
     return outParse
-standardize_phonetic("おじいーさん")
+
+def guessOnyomi(instr:str):
+    if is_katakana(instr):
+        return True
+    if is_kanji(instr) and len(instr)>1:
+        return True
+    return False
+
+boring_elements=[
+    "助詞-接続助詞"  # te/ta
+    "助詞",         # particles
+    "助詞-終助詞",  # sentence-ending na
+    "助動詞",       # desu,...
+    "格助詞"        # other particles (de, no)
+    "助動詞-ダ",    # MeCab-superfluous -da
+    "助詞-接続助詞",# object particles (ga,ni,wo)
+    "御",           # o-, go- honorofics
+    #"動詞-非自立可能", # suffix verbs
+]
+def bool_Interesting(entry, verbose=False):
+    element = " ".join(entry)
+    
+    for boring in boring_elements:
+        if boring in element:
+            #print(entry[0] +" was boring by "+boring)
+            return False
+    if("格助詞" in element):
+        # can't tell why this is necessary
+        return False
+
+    if("動詞-非自立可能") in element:
+        # suffix verbs are usually interesting
+        if entry[3]=="有る": # aru
+            return False
+        if entry[3]=="為る": # suru
+            return False
+        if entry[3]=="居る": # iru
+            return False
+    return True
 
 def deconjugate(inword:str):
     #TODO implement; probably its own class
     pass
+# %%
--- JihAnki/resources/ProcessEntry.py
+++ JihAnki/resources/ProcessEntry.py
@@ -1,8 +1,6 @@
 #%%
 import pandas as pd
 import MeCab
-import os
-
 import LangUtils
 tagger = MeCab.Tagger()
 
@@ -10,18 +8,11 @@ tagger = MeCab.Tagger()
 df = pd.read_excel("./ExampleBook.xlsx",header=0)
 df.fillna("", inplace=True)
 
-
-# %%
-import MeCab
 tagger = MeCab.Tagger()
-import os
 
 #debug
 import numpy as np
-
 import LangUtils as LU
-import ProcessEntry
-
 tagger = MeCab.Tagger()
 ### User override tag:
 # <!--user-->
@@ -29,21 +20,6 @@ overrideTag= "<!--user-->"
 
 df = pd.read_excel("./ExampleBook.xlsx",header=0)
 df.fillna("", inplace=True)
-
-#debug
-df2 = pd.DataFrame(data=np.zeros((10,len(df.columns))),\
-                    columns=df.columns.values)
-# %%
-### TODO: check if item should be treated
-
-#def process_entry(entry):
-idx = 2
-output = dict.fromkeys(df.columns.values)
-myentry = df.iloc[idx]
-
-
-output["imi"] = df.at[idx, "imi"]
-
 
 # tag pattern by line:
 # 0. identified piece ver batim
@@ -53,31 +29,147 @@ output["imi"] = df.at[idx, "imi"]
 # 4. Type of word
 # 5. ???
 # 6. (int) ???
+def recapcriteria(entry, hgDict=None):
+    # detect if the word is worth putting in the recap
+    if not LU.bool_Interesting(entry):
+        return False
+    if hgDict is not None:
+        if entry[3] in hgDict["fragments"]:
+            return True
+        else:
+            return False
+    return True
 
+
+def ExceptionFilter(entry):
+    # replacing MeCab's ideosyncracies with my own
+    # wo/o should show as wo during pronounciations
+    if entry[0]=="を":
+        entry[1]="を"
+        entry[2]="を"
+    # same with ha/wa
+    if entry[0]=="は":
+        entry[1]="は"
+        entry[2]="は"
+    return entry
 
 
 #%%
-def create_hyougen(idx:int):
+def create_hyougen(idx:int, debug=""):
+    # pass this dict to reibun creator for highlighting
+    reibunResource={
+        "hyougen"   : None,
+        "idx"       : idx,
+        "fragments" : []
+    }
     kirei_hyougen =""
     entry = df.iloc[idx].values
-    hyougen_yomikata=""
+    if debug != "":
+        entry=[None, debug]
+        print("ProcessEntry.create_hyougen warning - arg idx "+str(idx)+\
+              " overridden in favour of debug '"+debug+"'")
+        reibunResource["idx"]=None
     cleanHyou=entry[1].strip().strip("。")
+    reibunResource["hyougen"]=cleanHyou
     EntryParse = tagger.parse(cleanHyou).split("\n")
     for line in EntryParse:
         LineParse = line.split("\t")
-        if LineParse[0]=="EOS" or LineParse[0]==[""]:
+        if LineParse[0]=="EOS" or LineParse[0]==[""] or len(LineParse)<5:
             break
+        # for recognition in 
+        reibunResource["fragments"].append(LineParse[3])
         # special exception for some katakana words (ex. パンク)
         if len(LineParse)>=4:
             if "-" in LineParse[3]:
                 LineParse[3]=LineParse[3][:LineParse[3].find("-")]
         # add pronounciation to the card face
-        kirei_hyougen+="<br><b>"\
-            +LineParse[3]+"    --    "\
-            +LU.standardize_phonetic(LineParse[2])\
-            +"</b>"
+        if recapcriteria(LineParse):
+            kirei_hyougen+="<br><b>"\
+                +LineParse[3]+"    --    "\
+                +LU.standardize_phonetic(LineParse)\
+                +"</b>"
 
     kirei_hyougen="<b>"+cleanHyou+"</b><br>"+kirei_hyougen
-    return(kirei_hyougen)
 
-# %%
+    return kirei_hyougen, reibunResource
+
+
+
+
+
+
+def create_reibun(hyougenDict:dict, debug=""):
+    kirei_hyougen =""
+    entry = df.iloc[hyougenDict["idx"]].values
+    if hyougenDict["idx"] is None:
+        entry=[None, debug]
+        print("ProcessEntry.create_reibun warning - arg idx "+str(idx)+\
+              " overridden in favour of debug '"+debug+"'")
+    outsentence=""
+    pronounciation=""
+    recap=""
+    def hilitecriteria(entry,hgDict):
+        # guess-style
+        # 1. False in general
+        # 2. Interesting fragments are interesting
+        # 3. If a particle is in fragments and is right-adjacent
+        # to another highlight, then it's probably hyougen. 
+        if not entry[3] in hgDict["fragments"]:
+            return False
+        if LU.bool_Interesting(entry):
+            return True
+        if outsentence.endswith("</b>"):
+            return True
+        return False
+
+    phrase=df.iloc[hyougenDict["idx"]].values[4]
+    if phrase=="":
+        return ""
+    EntryParse=tagger.parse(phrase)
+
+    for line in EntryParse.split("\n"):
+        LineParse=line.split("\t")
+        if LineParse[0]=="EOS" or LineParse[0]==[""] or len(LineParse)<5:
+            break
+        LineParse=ExceptionFilter(LineParse)
+        Kakikata=LineParse[0]
+        if LU.is_katakana(LineParse[0]):
+            Yomikata=LineParse[1]
+        #if LU.guessOnyomi(LineParse[0]):
+            # Reasonable guess if it's onyomi
+            #Yomikata=LU.to_katakana(LU.standardize_phonetic(LineParse))
+        else:
+            # kunyomi
+            Yomikata=LU.standardize_phonetic(LineParse)
+            #print("yomikata = "+Yomikata)
+        
+        if hilitecriteria(LineParse,hyougenDict):
+            Kakikata = "<b>"+Kakikata+"</b>"
+            Yomikata = "<b>"+Yomikata+"</b>"
+            # stronger condition to add to recap
+
+            if recapcriteria(LineParse,hyougenDict):
+                recap+="<br><b>"+LineParse[3]\
+                    +"    --    "\
+                    +LU.standardize_phonetic(LineParse)\
+                    +r"<\b>"
+            else:
+                pass
+                #print(LineParse[0]+"failed to be interesting")
+        # always true
+        outsentence   +=Kakikata
+        pronounciation+=Yomikata
+    outsentence+="<br>"
+    outstring = outsentence+"<br>\n"+pronounciation+"<br>\n"+recap
+    outstring = outstring.replace("<b></b>","").replace("</b><b>","")
+    return outstring
+
+def ProcessEntry(idx:int):
+    hyougenface, HGdict = create_hyougen(773)
+    reibunface = create_reibun(HGdict)
+    return hyougenface, reibunface
+
+if __name__=="__main__":
+    cardface, HGdict = create_hyougen(773)
+    print(HGdict)
+    print(create_reibun(HGdict))
